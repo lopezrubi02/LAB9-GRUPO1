568b2066edd18df9a718a4eb29a79ab6e0b9fdeb
Emerson Munoz <munoz.e@pucp.edu.pe>
2021-06-21 20:09:56 -0500

    Proyect entity

--- src/main/java/com/example/lab9grupo1/entity/Proyect.java
+++ src/main/java/com/example/lab9grupo1/entity/Proyect.java
@@ -1,4 +1,23 @@
 package com.example.lab9grupo1.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="proyectos")
 public class Proyect {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idproyecto")
+    private int idproyecto;
+
+    @Column(name="nombreProyecto")
+    private String nombreProyecto;
+
+    @ManyToOne
+    @Column(name="usuario_owner")
+    private User usuarioOwner;
+
+
+
 }
