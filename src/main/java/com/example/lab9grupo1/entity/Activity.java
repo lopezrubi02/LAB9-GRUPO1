a60c610f2200ebc4e4a9df49fffa8f9e6538567f
Rubi Polanco <a20183238@pucp.edu.pe>
2021-06-21 20:09:05 -0500

    -

--- src/main/java/com/example/lab9grupo1/entity/Activity.java
+++ src/main/java/com/example/lab9grupo1/entity/Activity.java
@@ -1,4 +1,29 @@
 package com.example.lab9grupo1.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "actividades")
 public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idactividad;
+
+    @Column(name = "nombreActividad")
+    private String nombreactividad;
+
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name = "idproyecto")
+    private Proyect proyecto;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_owner")
+    private User usuarioowner;
+
+    private int peso;
+
+    private int estado;
 }
