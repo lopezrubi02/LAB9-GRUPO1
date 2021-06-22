82f356f8c6a89948bbe6bb1cb94b37bdc51f28ff
Rubi Polanco <a20183238@pucp.edu.pe>
2021-06-21 19:43:36 -0500

    user y area entity

--- src/main/java/com/example/lab9grupo1/entity/Area.java
+++ src/main/java/com/example/lab9grupo1/entity/Area.java
@@ -1,4 +1,32 @@
 package com.example.lab9grupo1.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "areas")
 public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idarea")
+    private int idarea;
+
+    @Column(name = "nombreArea")
+    private String nombrearea;
+
+    public int getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(int idarea) {
+        this.idarea = idarea;
+    }
+
+    public String getNombrearea() {
+        return nombrearea;
+    }
+
+    public void setNombrearea(String nombrearea) {
+        this.nombrearea = nombrearea;
+    }
 }
--- src/main/java/com/example/lab9grupo1/entity/User.java
+++ src/main/java/com/example/lab9grupo1/entity/User.java
@@ -1,12 +1,52 @@
 package com.example.lab9grupo1.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "usuarios")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "correo")
+    private String correo;
 
+    @ManyToOne
+    @JoinColumn(name = "idarea")
+    private Area area;
 
+    private String nombre;
+    private String apellidos;
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
 }
