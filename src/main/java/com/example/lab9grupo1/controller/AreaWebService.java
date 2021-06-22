aa7514d1278ea50db68151be544177069aa71e0f
Majo <a20181563@pucp.edu.pe>
2021-06-22 17:07:02 -0500

    area items a,b,f

--- src/main/java/com/example/lab9grupo1/controller/AreaWebService.java
+++ src/main/java/com/example/lab9grupo1/controller/AreaWebService.java
@@ -1,15 +1,17 @@
 package com.example.lab9grupo1.controller;
 
+import com.example.lab9grupo1.entity.Area;
 import com.example.lab9grupo1.repository.AreaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Optional;
 
 @Controller
 public class AreaWebService {
@@ -22,6 +24,95 @@ public class AreaWebService {
     public ResponseEntity obtenerListaAreas(){
 
         return new ResponseEntity(areaRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/area/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity obtenerArea(@PathVariable("id") String idStr) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        try {
+            int id = Integer.parseInt(idStr);
+            Optional<Area> opt = areaRepository.findById(id);
+            if (opt.isPresent()) {
+                responseMap.put("estado", "ok");
+                responseMap.put("area", opt.get());
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+            } else {
+                responseMap.put("estado", "error");
+                responseMap.put("msg", "no se encontró el area con id: " + id);
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+            }
+        } catch (NumberFormatException ex) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "El ID debe ser un número");
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //falta crear area
+    @PostMapping(value = "/area", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity guardarArea(
+            @RequestBody Area area,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        areaRepository.save(area);
+        if (fetchId) {
+            responseMap.put("id", area.getIdarea());
+        }
+        responseMap.put("estado", "creado");
+        return new ResponseEntity(responseMap, HttpStatus.CREATED);
+
+    }
+
+    //falta obtener area con lista ususarios
+
+    @PutMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity actualizarArea(@RequestBody Area area) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        if (area.getIdarea() > 0) {
+            Optional<Area> opt = areaRepository.findById(area.getIdarea());
+            if (opt.isPresent()) {
+                areaRepository.save(area);
+                responseMap.put("estado", "actualizado");
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+            } else {
+                responseMap.put("estado", "error");
+                responseMap.put("msg", "El area a actualizar no existe");
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Debe enviar un ID");
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(value = "/area/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity borrarArea(@PathVariable("id") String idStr) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        try {
+            int id = Integer.parseInt(idStr);
+            if (areaRepository.existsById(id)) {
+                areaRepository.deleteById(id);
+                responseMap.put("estado", "borrado exitoso");
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+            } else {
+                responseMap.put("estado", "error");
+                responseMap.put("msg", "no se encontró el area con id: " + id);
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+            }
+        } catch (NumberFormatException ex) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "El ID debe ser un número");
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+        }
     }
 
 
