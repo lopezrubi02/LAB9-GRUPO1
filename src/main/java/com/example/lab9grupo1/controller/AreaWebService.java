572797fe4fe3700c9f6e35c57ffd23b8bb1768df
Majo <a20181563@pucp.edu.pe>
2021-06-22 15:03:18 -0500

    area

--- src/main/java/com/example/lab9grupo1/controller/AreaWebService.java
+++ src/main/java/com/example/lab9grupo1/controller/AreaWebService.java
@@ -1,4 +1,29 @@
 package com.example.lab9grupo1.controller;
 
+import com.example.lab9grupo1.repository.AreaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
+
+@Controller
 public class AreaWebService {
+
+    @Autowired
+    AreaRepository areaRepository;
+
+    @GetMapping(value = "/area",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity obtenerListaAreas(){
+
+        return new ResponseEntity(areaRepository.findAll(), HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/java/com/example/lab9grupo1/repository/AreaRepository.java
+++ src/main/java/com/example/lab9grupo1/repository/AreaRepository.java
@@ -1,4 +1,9 @@
 package com.example.lab9grupo1.repository;
 
-public interface AreaRepository {
+import com.example.lab9grupo1.entity.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AreaRepository extends JpaRepository<Area, Integer> {
 }
